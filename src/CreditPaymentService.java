83af9c3ce94f18ae5eaa55ba5729480dfe2e3fc8
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-07-01 21:38:28 +0300

    gitignore added

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,11 @@
+public class CreditPaymentService {
+
+    public int calculate(int credit, double monthlyInterestRate, int numberOfMonth) {
+
+
+        double monthlyPaymentAmount = credit * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), numberOfMonth)) / (Math.pow((1 + monthlyInterestRate), numberOfMonth) - 1)));
+        return (int) monthlyPaymentAmount;
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        int credit = 1_000_000;
+        double monthlyInterestRate = 0.008325;
+        int numberOfMonth = 12;
+        float monthlyPaymentAmount = service.calculate(credit, monthlyInterestRate, numberOfMonth);
+
+        System.out.println(monthlyPaymentAmount);
+
+
+
+
+
+    }
+}
