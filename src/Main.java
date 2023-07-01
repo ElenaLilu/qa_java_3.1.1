c0e0124a71ee9918ee7534f1025a008ca90279c4
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-07-01 21:44:33 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int credit = 1_000_000;
         double monthlyInterestRate = 0.008325;
-        int numberOfMonth = 12;
+        int numberOfMonth = 24;
         float monthlyPaymentAmount = service.calculate(credit, monthlyInterestRate, numberOfMonth);
 
         System.out.println(monthlyPaymentAmount);
