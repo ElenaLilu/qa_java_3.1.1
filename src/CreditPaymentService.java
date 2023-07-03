db8eeaedebd8cdce5eb6d4d20059edd4160c8953
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-07-03 14:30:43 +0300

    src/Main.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
 
-    public int calculate(int credit, double monthlyInterestRate, int numberOfMonth) {
+    public int calculate(int credit, double monthlyInterestRate, int numberOfMonth, double annualInterestRate) {
 
 
         double monthlyPaymentAmount = credit * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), numberOfMonth)) / (Math.pow((1 + monthlyInterestRate), numberOfMonth) - 1)));
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,28 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
+
         int credit = 1_000_000;
         double annualInterestRate = 0.0999;
         double monthlyInterestRate = annualInterestRate / 12;
-        int numberOfMonth = 36;
-        float monthlyPaymentAmount = service.calculate(credit, monthlyInterestRate, numberOfMonth);
+        int numberOfMonth = 12;
+        float monthlyPaymentAmount = service.calculate(credit, monthlyInterestRate, numberOfMonth, annualInterestRate);
+        System.out.println(monthlyPaymentAmount);
 
+
+        credit = 1_000_000;
+        annualInterestRate = 0.0999;
+        monthlyInterestRate = annualInterestRate / 12;
+        numberOfMonth = 24;
+        monthlyPaymentAmount = service.calculate(credit, monthlyInterestRate, numberOfMonth, annualInterestRate);
+        System.out.println(monthlyPaymentAmount);
+
+
+        credit = 1_000_000;
+        annualInterestRate = 0.0999;
+        monthlyInterestRate = annualInterestRate / 12;
+        numberOfMonth = 36;
+        monthlyPaymentAmount = service.calculate(credit, monthlyInterestRate, numberOfMonth, annualInterestRate);
         System.out.println(monthlyPaymentAmount);
     }
 
